383d231d1d69c53f931b4c8155734645d4252fbc
armandnjapou <armand.njapoungassu@consulting-for.accor.com>
2025-03-04 22:50:47 +0100

    update clientlibs

--- ui.apps/src/main/content/jcr_root/apps/wknd/components/product/clientlib-product/js/product.js
+++ ui.apps/src/main/content/jcr_root/apps/wknd/components/product/clientlib-product/js/product.js
@@ -1,1 +1,9 @@
-console.log("Product")
+let productDiv = document.querySelector('.product');
+
+productDiv.addEventListener('click', () => {
+    fetch('https://dogapi.dog/api/v2/facts?limit=5')
+        .then(res => res.json())
+        .then(data => {
+            document.querySelector('.text-danger').innerText = data.attributes.body;
+        })
+})
